3a8280cbb4bf2c0471150c111a64ca48351c73c9
alpha <zzy_alpha@outlook.com>
2024-08-12 17:00:12 +0800

    功能增加、布局修改
    1、调整布局大小、增加清除消息框按钮、消息框标题
    2、增加着陆统计，优化计算代码、增加总体汇报、CAS汇报，cas汇报未完成，待修改

--- App_fparameter.py
+++ App_fparameter.py
@@ -30,8 +30,8 @@ class App(customtkinter.CTk):
         # real_screen_height = user32.GetSystemMetrics(1)
         # 由于geometry 大小为缩放后的像素大小，位置为真实像素位置，因此需要计算dpi恢复真实位置
         real_dpi = real_screen_width // screen_width
-        window_width = 500  # 窗口长度 系统DPI缩放后的值
-        window_height = 309  # 窗口高度
+        window_width = 1000  # 窗口长度 系统DPI缩放后的值
+        window_height = 618  # 窗口高度
         x = ((screen_width // 2) - (window_width // 2)) * real_dpi
         y = ((screen_height // 2) - (window_height // 2)) * real_dpi * 0.75  # 向上偏一点更好看
         self.geometry(f'{window_width}x{window_height}+{int(x)}+{int(y)}')
@@ -43,34 +43,47 @@ class App(customtkinter.CTk):
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="选择导出模式",
                                                  font=customtkinter.CTkFont(size=15, weight="bold"))
         # 侧边栏内按钮
-        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.single_button_event1,
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.single_button_event,
                                                         text="单个文件导出")
-        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.multi_button_event2,
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.multi_button_event,
                                                         text="多个文件导出")
+        self.sidebar_button_del_msg = customtkinter.CTkButton(self.sidebar_frame, command=self.del_button_event,
+                                                              text="重置信息框")
         # 文本框
         self.textbox = customtkinter.CTkTextbox(self, width=280)
+        self.logo_msg_box = customtkinter.CTkLabel(self, text="数据分析结果",
+                                                   font=customtkinter.CTkFont(size=15, weight="bold"))
 
         """部件外观"""
         # row行 column列 row span跨行 sticky网格单元格内对齐和扩展 padx=(20, 0)：这个参数是一个元组，指定了控件在水平方向上的内边距
+        # 第一列部件
         self.sidebar_frame.grid(row=0, column=0, rowspan=2, sticky="nsew")
-        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+        self.logo_label.grid(row=0, column=0, padx=0, pady=(20, 10))
         self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
         self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
-        self.textbox.grid(row=0, column=1, padx=20, pady=10, sticky="nsew")
-        """布局权重"""
-        self.grid_columnconfigure(0, weight=0)  # 设置第1列的行的权重为1
-        self.grid_rowconfigure(0, weight=1)  # 设置第1列的行的权重为1
+        self.sidebar_button_del_msg.grid(row=3, column=0, padx=20, pady=10)
+        # 第二列部件
+        self.logo_msg_box.grid(row=0, column=1, padx=0, pady=(20, 10))
+        self.textbox.grid(row=1, column=1, rowspan=2, padx=(20, 20), pady=(10, 10), sticky="nsew")
 
-    def single_button_event1(self):
+        """布局权重"""
+        self.grid_rowconfigure(1, weight=1)  # 行配置 index行号，weight权重
+        self.grid_columnconfigure(1, weight=1)  # 列配置 index列号，weight权重
+
+    def single_button_event(self):
         df, text_analyze, = ffp.single_abstract(self.df_idx)
-        # 清空现有内容 使用 "1.0" 作为插入文本的起始位置，这表示从文本框的第1行第0列开始插入内容。
-        # self.textbox.delete("1.0", customtkinter.END)
-        # 插入新的text_analyze内容
+        # 使用 "1.0" 作为插入文本的起始位置，这表示从文本框的第1行第0列开始插入内容。
         self.textbox.insert("1.0", text_analyze)
         return
 
-    def multi_button_event2(self):
+    def multi_button_event(self):
         ffp.multi_abstract(self.df_idx)
+        return
+
+    def del_button_event(self):
+        # 清空现有内容 使用 "1.0" 作为插入文本的起始位置，这表示从文本框的第1行第0列开始插入内容。
+        self.textbox.delete("1.0", customtkinter.END)
+        return
 
 
 if __name__ == "__main__":
--- func_fparameter.py
+++ func_fparameter.py
@@ -18,10 +18,9 @@ def single_abstract(df_idx):
         else:
             out_path = '提取数据.csv'  # 使用默认文件名
         df.to_csv(out_path, index=False, encoding='utf-8-sig')
-        text_analyze = single_analyze(df)
+        text_analyze = single_analyze(df, df_original)
     else:
-        return
-
+        text_analyze = "没有选择飞参文件\n"
     return df, text_analyze
 
 
@@ -60,18 +59,27 @@ def extract_flight_parameter(df_original, df_idx):
     return df
 
 
-def single_analyze(df):
+def single_analyze(df, df_original):
     """单文件数据分析"""
     # 起落架统计
     text_landing = landing_gear_analyze(df)
     # 开车时间统计
     text_engine = engine_analyze(df)
+    # 总体汇报
+    text_ps = performance_stability_analyze(df)
+    # CAS汇报
+    # text_cas = cas_analyze(df, df_original)
     # 总汇报
-    text_analyze = f"{text_landing}\n{text_engine}\n"
+    text_analyze = (f"{text_engine}\n"
+                    f"{text_landing}\n"
+                    f"{text_ps}\n"
+                    # f"{text_cas}\n"
+                    f"\n")
     return text_analyze
 
 
 def engine_analyze(df):
+    """动力专业分析"""
     engine_columns = ["1发转速", "2发转速", "3发转速", "4发转速"]
     # 提取每列的第一次和最后一次不为0的时间
     times = [(df.loc[df[df[col] != 0].index.min(), "飞行时间"],
@@ -80,7 +88,7 @@ def engine_analyze(df):
     if times:
         # 解压得到所有的第一次和最后一次时间列表，分别使用min和max来找出最早和最晚的时间
         first_times, last_times = zip(*times)
-        text = f"开车时间: {min(first_times)}, 关车时间: {max(last_times)}"
+        text = f"开车时间: {min(first_times)}，关车时间： {max(last_times)}"
     else:
         text = "本次数据分析飞机没有开车"
     return text
@@ -88,14 +96,69 @@ def engine_analyze(df):
 
 def landing_gear_analyze(df):
     """起落架专业分析"""
-    landing_gear_up = df['起落架收']
-    landing_gear_down = df['起落架放']
-    changes1 = landing_gear_up.diff(1) != 0
-    changes2 = landing_gear_down.diff(1) != 0
-    landing_gear_up = (changes1.sum() - 1) // 2
-    landing_gear_down = (changes2.sum() - 1) // 2
-    if landing_gear_up == landing_gear_down:
-        text = f'起落架收放次数为{landing_gear_up}'
+
+    def changes_count(landing_count):
+        changes = landing_count.diff(1) != 0
+        count = (changes.sum() - 1) // 2
+        return count
+
+    landing_gear_ups = changes_count(df['起落架收'])
+    landing_gear_downs = changes_count(df['起落架放'])
+    landing_load1 = changes_count(df['前轮载1'])
+    landing_load2 = changes_count(df['前轮载2'])
+    landing_load3 = changes_count(df['左主起轮载1'])
+    landing_load4 = changes_count(df['左主起轮载2'])
+    landing_load5 = changes_count(df['右主起轮载1'])
+    landing_load6 = changes_count(df['右主起轮载2'])
+    if landing_gear_ups == landing_gear_downs:
+        if landing_gear_ups in (landing_load1, landing_load2, landing_load3):
+            text = f'起落架收放 {landing_gear_ups} 次\n飞机着陆起降 {landing_gear_ups} 次'
+        else:
+            text = (f'起落架收放{landing_gear_ups}次\n '
+                    f'前起轮载1着陆{landing_load1}次、'
+                    f'前起轮载2着陆{landing_load2}次、'
+                    f'左主起轮载1着陆{landing_load3}次、'
+                    f'左主起轮载2着陆{landing_load4}次、'
+                    f'右主起轮载1着陆{landing_load5}次、'
+                    f'右主起轮载2着陆{landing_load6}次、')
     else:
         text = '起落架收放计数存在异常，请检查相关数据'
+    return text
+
+
+def performance_stability_analyze(df):
+    """性能操稳专业**简要分析**，起飞高度，最大飞行高度，飞行距离，最大飞行速度"""
+    text = " "
+    high_max = df['气压高度'].max()
+    high_min = df['气压高度'].min()
+    v_max = df['校准空速'].max()
+    v_min = df['校准空速'].min()
+    text = f'起飞高度为 {high_min} m，最大飞行高度为 {high_max} m，最大飞行速度为 {v_max} km/h'
+    return text
+
+
+def cas_analyze(df, df_original):
+    """告警分析，出现告警参数以及告警的时间段"""
+    text = " "
+    selected_columns = df_original.filter(like='显示告警系统').columns
+    # 存储结果
+    result = []
+    # 遍历每一个告警系统列
+    for column in selected_columns:
+        # 获取该列值为 1 的时间段
+        alarm_times = df[df[column] == 1]['飞行时间']
+
+        if not alarm_times.empty:
+            start_time = None
+
+            # 遍历每个时间点，找到连续时间段
+            for i, time in enumerate(alarm_times):
+                if start_time is None:
+                    start_time = time
+
+                # 如果下一个时间点与当前时间点不连续，则输出一个时间段
+                if i == len(alarm_times) - 1 or (alarm_times.iloc[i + 1] - time).seconds > 600:  # 超过10分钟不连续
+                    end_time = time
+                    result.append(f"{column}：告警时间从 {start_time} 到 {end_time}")
+                    start_time = None
     return text
